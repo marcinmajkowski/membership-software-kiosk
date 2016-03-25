c2f59e6a60868ea81ad441239f938ac965b6476e
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-03-25 06:21:50 +0100

    Init repository using Yeoman and generator-angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name membershipSoftwareKioskApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the membershipSoftwareKioskApp
+ */
+angular.module('membershipSoftwareKioskApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name membershipSoftwareKioskApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the membershipSoftwareKioskApp
+ */
+angular.module('membershipSoftwareKioskApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
